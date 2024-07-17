a73e6efc3d5c6bb175f6197f01a4019eb9e06d0b
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-07-17 13:22:56 +0900

    fix

--- common/src/main/java/net/pitan76/bedrocktools/mixin/AbstractBlockMixin.java
+++ common/src/main/java/net/pitan76/bedrocktools/mixin/AbstractBlockMixin.java
@@ -5,10 +5,11 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import net.pitan76.bedrocktools.item.BedrockPickaxeItem;
+import net.pitan76.mcpitanlib.api.entity.Player;
+import net.pitan76.mcpitanlib.api.util.BlockStateUtil;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,20 +20,22 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(AbstractBlock.class)
 public class AbstractBlockMixin {
     @Inject(method = "calcBlockBreakingDelta", at = @At(value = "JUMP", opcode = Opcodes.IFNE, shift = At.Shift.AFTER), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
-    public void inject_calcBlockBreakingDelta(BlockState state, PlayerEntity player, BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir, float hardness) {
-        ItemStack stack = player.getStackInHand(Hand.MAIN_HAND);
+    public void inject_calcBlockBreakingDelta(BlockState state, PlayerEntity playerEntity, BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir, float hardness) {
+        Player player = new Player(playerEntity);
+        ItemStack stack = player.getMainHandStack();
+
         if (hardness == -1.0F && stack.getItem() instanceof BedrockPickaxeItem) {
-            int effective = player.canHarvest(state) ? 30 : 100;
-            if (state.getBlock() == Blocks.BEDROCK) {
-                cir.setReturnValue(player.getBlockBreakingSpeed(state) / 20F / effective);
+            int effective = playerEntity.canHarvest(state) ? 30 : 100;
+            if (BlockStateUtil.getBlock(state) == Blocks.BEDROCK) {
+                cir.setReturnValue(playerEntity.getBlockBreakingSpeed(state) / 20F / effective);
                 return;
             }
-            if (state.getBlock() == Blocks.END_PORTAL_FRAME) {
-                cir.setReturnValue(player.getBlockBreakingSpeed(state) / 5F / effective);
+            if (BlockStateUtil.getBlock(state) == Blocks.END_PORTAL_FRAME) {
+                cir.setReturnValue(playerEntity.getBlockBreakingSpeed(state) / 5F / effective);
                 return;
             }
 
-            cir.setReturnValue(player.getBlockBreakingSpeed(state) / 10F / effective);
+            cir.setReturnValue(playerEntity.getBlockBreakingSpeed(state) / 10F / effective);
         }
     }
 }
